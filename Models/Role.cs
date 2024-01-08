a1536e1a66904b000096c45ec8eac5d7b969d2c3
hjbarangan <hjbarangan@newbrighton.edu.ph>
2024-01-08 18:16:10 +0800

    fix: Fixed cancel button in modal

--- Models/Role.cs
+++ Models/Role.cs
@@ -6,10 +6,13 @@ namespace dotnet_newbie.Models;
 public class Role
 {
     public int Id { get; set; }
+
+    [Required(ErrorMessage = "Role is required.")]
+    [Display(Name = "Role Name")]
     public string? Name { get; set; }
 
-   [Display(Name = "Date Created")]
-   [DataType(DataType.Date)]
+    [Display(Name = "Date Created")]
+    [DataType(DataType.Date)]
     public DateTime CreatedAt { get; set; }
 
 
